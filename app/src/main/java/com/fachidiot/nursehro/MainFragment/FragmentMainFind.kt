84b2681cb0362fce5dbb273139883c3a1aef78e9
fachidiot <jerrykim428>
2021-09-10 00:03:29 +0900

    fachidiot_window 09/10 sibal github

--- app/src/main/java/com/fachidiot/nursehro/MainFragment/FragmentMainFind.kt
+++ app/src/main/java/com/fachidiot/nursehro/MainFragment/FragmentMainFind.kt
@@ -12,13 +12,16 @@ import android.location.Location
 import android.os.Bundle
 import android.util.DisplayMetrics
 import android.util.Log
-import android.view.*
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.fachidiot.nursehro.Class.CustomCluserRenderer
 import com.fachidiot.nursehro.Class.CustomUserInfo
 import com.fachidiot.nursehro.Class.LatLngUser
 import com.fachidiot.nursehro.Class.UserList
@@ -26,13 +29,13 @@ import com.fachidiot.nursehro.MapList_Adapter
 import com.fachidiot.nursehro.R
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
-import com.google.android.gms.maps.*
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.libraries.places.api.Places
-import com.google.android.libraries.places.api.model.Place
-import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest
 import com.google.android.libraries.places.api.net.PlacesClient
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -40,14 +43,12 @@ import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
 import com.google.maps.android.clustering.ClusterManager
+import com.google.maps.android.clustering.view.ClusterRenderer
 import kotlinx.android.synthetic.main.fragment_main_find.*
 import java.util.*
 
-
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 class FragmentMainFind : Fragment(), OnMapReadyCallback {
+
     private var profileList : ArrayList<UserList> = ArrayList()
 
     private lateinit var mFirebaseAuth : FirebaseAuth
@@ -60,7 +61,8 @@ class FragmentMainFind : Fragment(), OnMapReadyCallback {
     private val defaultLocation = LatLng(37.557667, 126.926546)
     private var lastKnownLocation: Location? = null
 
-    private lateinit var clusterManager: ClusterManager<LatLngUser>
+    private var clusterManager: ClusterManager<LatLngUser>? = null
+    private var clusterRenderer: ClusterRenderer<LatLngUser>? = null
 
     private var likelyPlaceNames: Array<String?> = arrayOfNulls(0)
     private var likelyPlaceAddresses: Array<String?> = arrayOfNulls(0)
@@ -86,22 +88,16 @@ class FragmentMainFind : Fragment(), OnMapReadyCallback {
         mFirebaseStoreDatabase = Firebase.firestore
         mFirebaseStorage = FirebaseStorage.getInstance()
         mFirebaseAuth = FirebaseAuth.getInstance()
-
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
-
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        setAddressMarker()
+        //setAddressMarker()
+        setUserList()
 
         ListButton.setOnClickListener {
             Map_ListLayout.visibility = View.VISIBLE
-            setUserList()
         }
 
         button_back.setOnClickListener {
@@ -130,7 +126,7 @@ class FragmentMainFind : Fragment(), OnMapReadyCallback {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-        ): View? {
+    ): View? {
         // Inflate the layout for this fragment
         val rootView = inflater.inflate(R.layout.fragment_main_find, container, false)
         mapView = rootView.findViewById(R.id.map) as MapView
@@ -139,15 +135,29 @@ class FragmentMainFind : Fragment(), OnMapReadyCallback {
         return rootView
     }
 
-
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-
-        val Seoul = LatLng(37.557667, 126.926546)
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(Seoul))
-        googleMap.moveCamera(CameraUpdateFactory.zoomTo(15f))
-
         setCustomMarkerView()
+
+        clusterManager = ClusterManager<LatLngUser>(context, mMap)
+        clusterRenderer = CustomCluserRenderer(context, mMap, clusterManager, markerRootView)
+        clusterManager!!.setRenderer(clusterRenderer)
+
+        mMap.setOnMapLoadedCallback {
+            val seoul = LatLng(37.557667, 126.926546)
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(seoul))
+            mMap.animateCamera(CameraUpdateFactory.zoomTo(13f))
+        }
+
+        val mPreviousCameraPosition = arrayOf<CameraPosition?>(null)
+        mMap.setOnCameraIdleListener {
+            val position = mMap.cameraPosition
+            if (mPreviousCameraPosition[0] == null || mPreviousCameraPosition[0]!!.zoom != position.zoom) {
+                mPreviousCameraPosition[0] = mMap.cameraPosition
+                clusterManager!!.cluster()
+            }
+        }
+        mMap.setOnMarkerClickListener(clusterManager)
 
         if (context?.let {
                 ActivityCompat.checkSelfPermission(
@@ -182,35 +192,6 @@ class FragmentMainFind : Fragment(), OnMapReadyCallback {
         getDeviceLocation()
     }
 
-    private fun getDeviceLocation() {
-        try {
-            if (locationPermissionGranted) {
-                val locationResult = fusedLocationProviderClient.lastLocation
-                locationResult.addOnCompleteListener(requireActivity()) { task ->
-                    if (task.isSuccessful) {
-                        // Set the map's camera position to the current location of the device.
-                        lastKnownLocation = task.result
-                        if (lastKnownLocation != null) {
-                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
-                                LatLng(lastKnownLocation!!.latitude,
-                                    lastKnownLocation!!.longitude), DEFAULT_ZOOM.toFloat()))
-
-                        }
-                    } else {
-                        Log.d("Map/GetDeviceLocation", "Current location is null. Using defaults.")
-                        Log.e("Map/GetDeviceLocation", "Exception: %s", task.exception)
-                        mMap.moveCamera(CameraUpdateFactory
-                            .newLatLngZoom(defaultLocation, DEFAULT_ZOOM.toFloat()))
-
-                        mMap.uiSettings.isMyLocationButtonEnabled = false
-                    }
-                }
-            }
-        } catch (e: SecurityException) {
-            Log.e("Exception: %s", e.message, e)
-        }
-    }
-
     // 구글맵 주소 검색 메서드 근처 위치의 간호사 유저 정보를 가져와야함
     private fun search(addresses: List<Address>) {
         val address: Address = addresses[0]
@@ -221,81 +202,11 @@ class FragmentMainFind : Fragment(), OnMapReadyCallback {
         mMap.animateCamera(CameraUpdateFactory.zoomTo(15f))
     }
 
-    private fun setAddressMarker() {
-        mFirebaseStoreDatabase.collection("users").whereEqualTo("nurse",  true).orderBy("location").get().addOnSuccessListener {
-            for (dc in it.documents) {
-                val user = dc.toObject(CustomUserInfo::class.java)
-                if (user != null) {
-                    val geocoder: Geocoder = Geocoder(requireContext(), Locale.getDefault())
-
-                    val location = geocoder.getFromLocationName(user.location, 3)
-                    val latLng = LatLng(location[0].latitude, location[0].longitude)
-
-                    Log.d("setAddressMarker", user.location.toString())
-                    val markerOptions = MarkerOptions()
-                    markerOptions.position(latLng)
-                    marker_body.text = user.location
-                    marker_num.text = profileList.count().toString()
-                    markerOptions.icon(BitmapDescriptorFactory.fromBitmap(createDrawableFromView(requireContext(), markerRootView)))
-                    mMap.addMarker(markerOptions)
-                }
-            }
-        }
-    }
-
-    // 현위치의 리스트
-    private fun onGetUserList(location : String, latLng : LatLng? = null) {
-        profileList.clear()
-        mFirebaseStoreDatabase.collection("users").whereEqualTo("nurse",  true).whereArrayContains("location", location).get()
-            .addOnSuccessListener{
-                for (dc in it.documents) {
-                    val user = dc.toObject(CustomUserInfo::class.java)
-                    if (user != null) {
-                        profileList.add(UserList(user.userNickname, user.profileImage, user.location, user.sex, user.age))
-                    }
-                }
-            }
-    }
-
     private fun setCustomMarkerView() {
         markerRootView = LayoutInflater.from(context).inflate(R.layout.custom_marker, null)
-        marker_body = markerRootView.findViewById(R.id.tv_marker_body) as TextView
-        marker_num = markerRootView.findViewById(R.id.tv_marker_num) as TextView
+        marker_body = markerRootView.findViewById(R.id.tv_marker_body)
+        marker_num = markerRootView.findViewById(R.id.tv_marker_num)
     }
-
-
-
-    private fun setUpClusterer() {
-        // Initialize the manager with the context and the map.
-        // (Activity extends context, so we can pass 'this' in the constructor.)
-        clusterManager = ClusterManager(context, mMap)
-
-        // Point the map's listeners at the listeners implemented by the cluster
-        // manager.
-        mMap.setOnCameraIdleListener(clusterManager)
-        mMap.setOnMarkerClickListener(clusterManager)
-
-        // Add cluster items (markers) to the cluster manager.
-        addItems()
-    }
-
-    private fun addItems() {
-
-        // Set some lat/lng coordinates to start with.
-        var lat = 51.5145160
-        var lng = -0.1270060
-
-        // Add ten cluster items in close proximity, for purposes of this example.
-        for (i in 0..9) {
-            val offset = i / 60.0
-            lat += offset
-            lng += offset
-            val offsetItem =
-                    LatLngUser(lat, lng, "Title $i", "Snippet $i")
-            clusterManager.addItem(offsetItem)
-        }
-    }
-
 
     // View를 Bitmap으로 변환
     private fun createDrawableFromView(context: Context, view: View): Bitmap? {
@@ -312,11 +223,33 @@ class FragmentMainFind : Fragment(), OnMapReadyCallback {
     }
 
     private fun setUserList() {
-        MapRecyclerView.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false) //레이아웃매니저를 이용해 어뎁터의 방향을 결정
-        MapRecyclerView.setHasFixedSize(true)//어뎁터에 성능을 위한것
-        MapRecyclerView.adapter = MapList_Adapter(profileList) //어뎁터에 리스트 자료를 넣는다.
-    }
+        profileList.clear()
 
+        mFirebaseStoreDatabase.collection("users").whereEqualTo("nurse", true).get()
+            .addOnCompleteListener{
+                if(it.isSuccessful) {
+                    val geocoder = Geocoder(context)
+                    for (dc in it.result!!.documents) {
+                        val user = dc.toObject(CustomUserInfo::class.java)
+                        if (user != null) {
+                            profileList.add(UserList(user.userNickname, user.profileImage, user.location, user.sex, user.age))
+
+                            val addresses: List<Address> = geocoder.getFromLocationName(user.location.toString(), 3)
+                            val address : Address = addresses[0]
+                            val latLng = LatLng(address.latitude, address.longitude)
+                            val offsetItem = LatLngUser(user.uid, LatLng(latLng.latitude, latLng.longitude), user.location.toString())
+                            clusterManager?.addItem(offsetItem)
+                        }
+                    }
+                    MapRecyclerView.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false) //레이아웃매니저를 이용해 어뎁터의 방향을 결정
+                    MapRecyclerView.setHasFixedSize(true)//어뎁터에 성능을 위한것
+                    MapRecyclerView.adapter = MapList_Adapter(profileList) //어뎁터에 리스트 자료를 넣는다.
+                }
+            }
+            .addOnFailureListener {
+                Toast.makeText(context, "Failed to load recommend userlist", Toast.LENGTH_SHORT).show()
+            }
+    }
 
     override fun onStart() {
         super.onStart()
@@ -348,6 +281,35 @@ class FragmentMainFind : Fragment(), OnMapReadyCallback {
         mapView?.onLowMemory()
     }
 
+
+    private fun getDeviceLocation() {
+        try {
+            if (locationPermissionGranted) {
+                val locationResult = fusedLocationProviderClient.lastLocation
+                locationResult.addOnCompleteListener(requireActivity()) { task ->
+                    if (task.isSuccessful) {
+                        // Set the map's camera position to the current location of the device.
+                        lastKnownLocation = task.result
+                        if (lastKnownLocation != null) {
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                LatLng(lastKnownLocation!!.latitude,
+                                    lastKnownLocation!!.longitude), DEFAULT_ZOOM.toFloat()))
+
+                        }
+                    } else {
+                        Log.d("Map/GetDeviceLocation", "Current location is null. Using defaults.")
+                        Log.e("Map/GetDeviceLocation", "Exception: %s", task.exception)
+                        mMap.moveCamera(CameraUpdateFactory
+                            .newLatLngZoom(defaultLocation, DEFAULT_ZOOM.toFloat()))
+
+                        mMap.uiSettings.isMyLocationButtonEnabled = false
+                    }
+                }
+            }
+        } catch (e: SecurityException) {
+            Log.e("Exception: %s", e.message, e)
+        }
+    }
 
     //TODO : PermissionCheck
     private fun getLocationPermission() {
@@ -404,14 +366,5 @@ class FragmentMainFind : Fragment(), OnMapReadyCallback {
         private const val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
         private const val DEFAULT_ZOOM = 15
         private const val M_MAX_ENTRIES = 5
-
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FragmentMainFind().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
